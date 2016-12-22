b130aec1e24d8fe1b6ad9734ff8fb9d071dfbe0d
swsvc <swsvc@example.com>
2016-12-22 17:06:14 +0300

    adding admin interface

--- photogallery/photo/admin.py
+++ photogallery/photo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Tag, Photo, Like
+
+class TagAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Tag
+
+    list_display = ('name', 'is_blocking')
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Photo
+
+    list_display = ('created_date', 'href', 'blocked_by_tag')
+
+admin.site.register(User)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Like)
